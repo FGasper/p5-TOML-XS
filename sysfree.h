1f116c4717986a1971d5dbf5d0352300c813d0c3
Felipe Gasper <felipe@felipegasper.com>
2021-04-09 00:43:57 -0400

    comment

--- sysfree.h
+++ sysfree.h
@@ -1,1 +1,6 @@
+/* On some systems Perl clobbers free() to be its own special thing.
+   That doesn’t work very well with tomlc99’s expectation that we call
+   free() on some of its stuff. This works around that by providing a
+   means to call free() from an XSUB.
+*/
 void tomlxs_sysfree(void *ptr);
